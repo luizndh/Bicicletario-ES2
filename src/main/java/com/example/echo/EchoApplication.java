70b79353117c10c3208965344c52dd75ea23b713
Breno Figueiredo Borges <brenofigueiredoborges@gmail.com>
2023-10-23 19:10:00 -0300

    configurando deploy

--- src/main/java/com/example/echo/EchoApplication.java
+++ src/main/java/com/example/echo/EchoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EchoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EchoApplication.class, args); 
+		SpringApplication.run(EchoApplication.class, args);
+		
 	}
 
 }
