27358a13a280996d0c11c19283675bf257c6fcb0
lclcada <lucas.arruda@edu.unirio.br>
2023-11-14 13:22:45 -0300

    testes

---
+++ Externo/src/test/java/com/externo/controller/CartaoDeCreditoControllerTest.java
@@ -0,0 +1,20 @@
+package com.externo.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.aluguel.model.CartaoDeCredito;
+import com.aluguel.service.CartaoDeCreditoService;
+
+@ExtendWith(MockitoExtension.class)
+public class CartaoDeCreditoControllerTest {
+
+}
