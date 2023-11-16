205912fed3caa8b3ef28d9e22323de7ee5d19646
lclcada <lucas.arruda@edu.unirio.br>
2023-11-16 20:22:47 -0300

    testes e inicio integracao cobranca

--- Externo/src/test/java/com/externo/servico/CobrancaServiceTest.java
+++ Externo/src/test/java/com/externo/servico/CobrancaServiceTest.java
@@ -1,5 +1,7 @@
 package com.externo.servico;
 
+import com.externo.dto.CartaoDeCreditoDTO;
+import com.externo.model.Cobranca;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import com.externo.dto.CobrancaDTO;
 import com.externo.servico.CobrancaService;
@@ -16,6 +19,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CobrancaServiceTest {
+    @Mock
+    private CobrancaService cobrancaService;
+
+    @Mock
+    private List<Cobranca> cobrancas;
+
+    @BeforeEach
+    void setUp() {
+        // Arrange
+        CobrancaDTO dto1 = new CobrancaDTO(Cobranca.StatusCobranca.PAGA, "2021-05-01T12:00:00", "2021-05-01T12:00:00", 10.0f, 1);
+        CobrancaDTO dto2 = new CobrancaDTO(Cobranca.StatusCobranca.FALHA, "2021-05-01T12:00:00", "2021-05-01T12:00:00", 10.0f, 2);
+        CobrancaDTO dto3 = new CobrancaDTO(Cobranca.StatusCobranca.PENDENTE, "2021-05-01T12:00:00", "2021-05-01T12:00:00", 10.0f, 3);
+        CobrancaDTO dto4 = new CobrancaDTO(Cobranca.StatusCobranca.PENDENTE, "2021-05-01T12:00:00", "2021-05-01T12:00:00", 10.0f, 4);
+
+        cobrancas.add(new Cobranca(dto1));
+        cobrancas.add(new Cobranca(dto2));
+        cobrancas.add(new Cobranca(dto3));
+        cobrancas.add(new Cobranca(dto4));
+    }
+
+    @Test
+    void testObterCobrancaInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> cobrancaService.obterCobranca(-1));
+    }
+
+    @Test
+    void testObterCobrancaQueNaoExiste() {
+        assertThrows(NoSuchElementException.class, () -> cobrancaService.obterCobranca(5));
+    }
+
+    @Test
+    void testObterCobranca() {
+        // Act
+        Cobranca cobranca = cobrancaService.obterCobranca(1);
+
+        // Assert
+        assertNotNull(cobranca);
+        assertEquals(cobranca.getId(), 1);
+        assertEquals(cobranca.getStatus(), Cobranca.StatusCobranca.PAGA);
+    }
+
+    //TODO adicionar o resto dos testes:
+    // - testar se a cobranca foi adicionada na fila
+    // - testar se a cobranca foi processada e aprovada
+    // - testar se a cobranca foi processada e reprovada
 
 
 }
